074fce6f14af89a9dca4396cbe0ebcddb6e54ade
lrecfor <lalarecfor@gmail.com>
2023-10-09 11:52:47 +0500

    finished app interface

--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,23 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GObject
+from utils import *
+import scanner
+
+
+def parse_number_string(input_string):
+    parts = input_string.split(',')
+    numbers = []
+
+    for part in parts:
+        if '-' in part:
+            start, end = map(int, part.split('-'))
+            numbers.extend(range(start, end + 1))
+        else:
+            numbers.append(int(part.strip()))
+
+    return numbers
+
 
 class MainWindow:
     def __init__(self):
@@ -19,15 +36,16 @@ class MainWindow:
 
         # Создаем RadioButton для выбора между "Default" и "Custom"
         self.default_radio = Gtk.RadioButton.new_with_label_from_widget(None, "Default")
-        self.custom_radio = Gtk.RadioButton.new_with_label_from_widget(self.default_radio, "Custom")
+        self.custom_radio = Gtk.RadioButton.new_from_widget(self.default_radio)
+        self.custom_radio.set_label("Custom")
 
         self.custom_radio.connect("toggled", self.on_custom_toggled)
 
         self.submit_button = Gtk.Button(label="Submit")
         self.output_edit = Gtk.TextView()
         self.output_edit.set_editable(False)
-        self.output_edit.set_wrap_mode(Gtk.WrapMode.NONE)
-        self.output_edit.set_cursor_visible(True)
+        self.output_edit.set_wrap_mode(Gtk.WrapMode.WORD)
+        self.output_edit.set_cursor_visible(False)
 
         self.submit_button.connect("clicked", self.start_scan)
 
@@ -41,6 +59,10 @@ class MainWindow:
         context = self.output_edit.get_style_context()
         context.add_provider(style_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
+        self.output_scroll = Gtk.ScrolledWindow()
+        self.output_scroll.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        self.output_scroll.add(self.output_edit)
+
         self.main_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=8)
         self.main_box.set_margin_start(8)
         self.window.add(self.main_box)
@@ -50,21 +72,25 @@ class MainWindow:
         self.left_box.set_margin_top(8)
         self.main_box.pack_start(self.left_box, False, True, 0)
 
+        self.left_box.pack_start(Gtk.Label('Host:', xalign=0.0), False, True, 0)
         self.left_box.pack_start(self.host_edit, False, True, 0)
+        self.left_box.pack_start(Gtk.Label('Scan type:', xalign=0.0), False, True, 0)
         self.left_box.pack_start(self.scan_type_combo, False, True, 0)
 
         # Добавляем RadioButton "Default" и "Custom"
+        self.left_box.pack_start(Gtk.Label('Ports:', xalign=0.0), False, True, 0)
         self.left_box.pack_start(self.default_radio, False, True, 0)
         self.left_box.pack_start(self.custom_radio, False, True, 0)
 
         # Создаем поле ввода только для "Custom"
-        self.username_entry = Gtk.Entry()
-        self.left_box.pack_start(self.username_entry, False, True, 0)
-        self.username_entry.set_sensitive(False)  # Изначально отключено
+        self.ports_edit = Gtk.Entry()
+        self.ports_edit.set_text(", ".join([str(i) for i in default_ports]))  # Устанавливаем изначальное значение
+        self.left_box.pack_start(self.ports_edit, False, True, 0)
+        self.ports_edit.set_sensitive(False)  # Изначально отключено
 
         self.left_box.pack_start(self.submit_button, False, True, 0)
 
-        self.main_box.pack_start(self.output_edit, True, True, 0)
+        self.main_box.pack_start(self.output_scroll, True, True, 0)
         self.output_edit.set_margin_top(8)
         self.output_edit.set_margin_end(8)
         self.output_edit.set_margin_bottom(8)
@@ -72,26 +98,48 @@ class MainWindow:
         # Создаем обработчик клавиш для приложения
         self.accel_group = Gtk.AccelGroup()
         self.window.add_accel_group(self.accel_group)
-        key, modifier = Gtk.accelerator_parse("Return")  # Клавиша Enter
+        key, modifier = Gtk.accelerator_parse("Return")
         self.accel_group.connect(key, modifier, Gtk.AccelFlags.VISIBLE, self.on_enter_key_pressed)
 
         self.window.show_all()
 
     def on_custom_toggled(self, button):
         # Отключаем/включаем поле ввода при переключении состояния RadioButton "Custom"
-        self.username_entry.set_sensitive(button.get_active())
+        self.ports_edit.set_sensitive(button.get_active())
+
+        # Если выбран "Default", устанавливаем значение default_ports в поле ввода "Ports"
+        if not button.get_active():
+            self.ports_edit.set_text(", ".join([str(i) for i in default_ports]))
+        else:
+            self.ports_edit.set_text("")
 
     def start_scan(self, widget):
-        host = self.host_edit.get_text()
-        scan_type = self.scan_type_combo.get_active_text()
-        username = self.username_entry.get_text() if self.username_entry.get_sensitive() else ""  # Получаем текст из поля ввода только при включенном состоянии
-
-        if not host:
-            self.output_result("Host is not specified")
+        try:
+            host = self.host_edit.get_text()
+            if not host:
+                raise HostNotSpecifiedException("Host is not specified")
+        except HostNotSpecifiedException as e:
+            self.output_result(str(e))
             return
 
-        scan_info = f"Host: {host}\nScan Type: {scan_type}\nUsername: {username}"
-        self.output_result(scan_info)
+        host = host.split(', ')
+        scan_type = self.scan_type_combo.get_active_text()
+        ports_ = parse_number_string(self.ports_edit.get_text()) if self.custom_radio.get_active() else "default"
+
+        try:
+            if scan_type == "TCP":
+                ports_ = scanner.tcp_scan(host, ports_)
+            elif scan_type == "UDP":
+                ports_ = scanner.udp_scan(host, ports_)
+            elif scan_type == "FIN":
+                ports_ = scanner.fin_scan(host, ports_)
+            elif scan_type == "SYN":
+                ports_ = scanner.syn_scan(host, ports_)
+
+            self.output_result("PORT\t\tSTATUS\n" + ports_)
+            # db.add(Scan(host=host, ports=ports))
+        except ScanErrorException as e:
+            self.output_result("Error occurred")
 
     def output_result(self, text):
         buffer = self.output_edit.get_buffer()
@@ -100,7 +148,3 @@ class MainWindow:
     def on_enter_key_pressed(self, *args):
         # Вызываем нажатие кнопки "Submit" при нажатии клавиши Enter
         self.submit_button.clicked()
-
-if __name__ == "__main__":
-    app = MainWindow()
-    Gtk.main()
